f27373f8db8907a35d45e596c9cb7b0eb57eba24
AmaaniL <malfait99+github@gmail.com>
2025-12-19 17:10:46 +0100

    feat(workitem): add support for active task tracking in WorkItem

--- src/workitem.ts
+++ src/workitem.ts
@@ -1,19 +1,25 @@
-import {TFile} from "obsidian";
-import {PomoTaskItem} from "./pomo_task_item";
+import { TFile } from "obsidian";
+import { PomoTaskItem } from "./pomo_task_item";
 
 export class WorkItem {
     activeNote: TFile;
-    initialPomoTaskItems : PomoTaskItem[];
-    postPomoTaskItems:PomoTaskItem[];
+    initialPomoTaskItems: PomoTaskItem[];
+    postPomoTaskItems: PomoTaskItem[];
     modifiedPomoTaskItems: PomoTaskItem[];
     isStartedActiveNote: boolean;
-    hasActiveTask: boolean;
+
+    timedTasks: PomoTaskItem[];
+    activeTask: PomoTaskItem | null;
 
     constructor(activeNote: TFile, isStartedActiveNote: boolean) {
         this.activeNote = activeNote;
-        this.initialPomoTaskItems = new Array<PomoTaskItem>();
-        this.postPomoTaskItems = new Array<PomoTaskItem>();
-        this.modifiedPomoTaskItems = new Array<PomoTaskItem>();
+        this.initialPomoTaskItems = [];
+        this.postPomoTaskItems = [];
+        this.modifiedPomoTaskItems = [];
         this.isStartedActiveNote = isStartedActiveNote;
+
+        
+        this.timedTasks = [];
+        this.activeTask = null;
     }
 }
